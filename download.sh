9266caaebb73dabd43acd5abd5de6a2445d974ca
joangui <joan@ac.upc.edu>
2018-10-02 11:46:01 +0200

    My-TRAC folder herarchy created

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/My-TRAC/My-TRAC-Platform.git
+cd My-TRAC-Platform
+git clone https://github.com/My-TRAC/data-model.git
+cd ..
+git clone https://github.com/My-TRAC/My-TRAC-Companion.git
+git clone https://github.com/My-TRAC/ThirdParty-Services.git
+git clone https://github.com/My-TRAC/Operators-Portal.git
+

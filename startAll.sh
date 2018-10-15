b4d35e995a5ef04d4e45c7c1d5ee25d6736060bf
joangui <joan@ac.upc.edu>
2018-10-15 16:25:29 +0200

    startAll.sh stopAll.sh modified to start/stop operators platform

--- startAll.sh
+++ startAll.sh
@@ -24,8 +24,10 @@ do
         ;;
     -d|--deployment)
 	MY_TRAC_DEPLOYMENT="$2"
-	if [[ $MY_TRAC_DEPLOYMENT != "docker-compose" ]] 
+	if [[ $MY_TRAC_DEPLOYMENT = "docker-compose" ]] 
 	then
+        touch .docker-compose
+    else
 		echo "Invalid deployment type \"${MY_TRAC_DEPLOYMENT}\""
 		exit 1 
 	fi
@@ -51,6 +53,10 @@ then
     ./start.sh
     cd $CURRENT_PATH
     cd My-TRAC-Companion/examples/CSVToKafkaTopic/deployments/${MY_TRAC_DEPLOYMENT}
+    ./start.sh
+
+    cd $CURRENT_PATH
+    cd Operators-Portal/examples/RatingsVisualizer/deployments/${MY_TRAC_DEPLOYMENT}
     ./start.sh
 else
     echo "Not implemented yet"
--- stopAll.sh
+++ stopAll.sh
@@ -4,11 +4,24 @@ set -e
 set -o pipefail
             
 CURRENT_PATH=`pwd`
+if [ -f .docker-compose ]; 
+then
+    MY_TRAC_DEPLOYMENT=docker-compose
+    rm .docker-compose
+fi
+
 if [[ -n ${MY_TRAC_DEPLOYMENT} && ${MY_TRAC_DEPLOYMENT} == "docker-compose" ]]
 then 
+    # STOP My-TRAC Companion Components
     cd My-TRAC-Companion/examples/CSVToKafkaTopic/deployments/${MY_TRAC_DEPLOYMENT}
-    ls
     ./stop.sh 
+
+    #STOP Operators Portal Components
+    cd $CURRENT_PATH
+    cd Operators-Portal/examples/RatingsVisualizer/deployments/${MY_TRAC_DEPLOYMENT}
+    ./stop.sh 
+
+    #STOP PLATFORM
     cd $CURRENT_PATH
     cd My-TRAC-Platform/deployments/${MY_TRAC_DEPLOYMENT}
     ./stop.sh

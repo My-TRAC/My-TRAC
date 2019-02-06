392c7a5f60352a37b04baa4d354460a946c5d2f3
ArnauPrat <arnau.prat@gmail.com>
2019-02-06 15:00:35 +0100

    Added scripts to start/stop cluster at gce

---
+++ startGCECluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gcloud container --project "buoyant-episode-216912" \
+  clusters create "mytrac-cluster-1" \
+  --zone "europe-west1-b" \
+  --username "admin" \
+  --cluster-version "1.11.6-gke.2" \
+  --machine-type "n1-standard-4" \
+  --image-type "COS" \
+  --disk-type "pd-standard" \
+  --disk-size "100" \
+  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append"\
+  --num-nodes "3" \
+  --enable-cloud-logging \
+  --enable-cloud-monitoring \
+  --no-enable-ip-alias \
+  --network "projects/buoyant-episode-216912/global/networks/default" \
+  --subnetwork "projects/buoyant-episode-216912/regions/europe-west1/subnetworks/default" \
+  --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+  --enable-autoupgrade \
+  --enable-autorepair

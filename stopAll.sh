8c315653044b8dfda2aaaef5ebce969b4bf92a23
ArnauPrat <arnau.prat@gmail.com>
2019-02-12 16:49:46 +0100

    improved stopall script to allow a list of components to stop

--- stopAll.sh
+++ stopAll.sh
@@ -18,25 +18,26 @@ then
 fi
 
 
+# LIST OF COMPONENTS TO START
+COMPONENTS_LIST="
+My-TRAC-Platform 
+My-TRAC-Companion/examples/CSVToKafkaTopic
+Operators-Portal/examples/RatingsVisualizer
+"
+
 if [[ -n ${MY_TRAC_DEPLOYMENT} && ${MY_TRAC_DEPLOYMENT} == "docker-compose" || ${MY_TRAC_DEPLOYMENT} == "kubernetes" ]]
 then 
-    # STOP My-TRAC Companion Components
-    cd My-TRAC-Companion/examples/CSVToKafkaTopic/deployments/${MY_TRAC_DEPLOYMENT}
-    ./stop.sh 
 
-    #STOP Operators Portal Components
-    cd $CURRENT_PATH
-    cd Operators-Portal/examples/RatingsVisualizer/deployments/${MY_TRAC_DEPLOYMENT}
+  CURRENT_PATH=`pwd`
+
+  for COMPONENT in ${COMPONENTS_LIST}
+  do
+    echo "Deploying ${COMPONENT}"
+    cd ${COMPONENT}/deployments/${MY_TRAC_DEPLOYMENT}
     ./stop.sh
+    cd ${CURRENT_PATH}
+  done
 
-    #STOP PLATFORM
-#    cd $CURRENT_PATH
-#    cd My-TRAC-Platform/examples/RatingModeler/deployments/${MY_TRAC_DEPLOYMENT} 
-#    ./stop.sh 
-
-    cd $CURRENT_PATH
-    cd My-TRAC-Platform/deployments/${MY_TRAC_DEPLOYMENT}
-    ./stop.sh
 else
     echo "Not implemented yet"
 fi
